d8ce316a0871202469a3744ebddd2a1c3b507531
iway1 <iway1@vols.utk.edu>
2018-02-25 16:37:22 -0500

    ...

--- AICA.py
+++ AICA.py
@@ -1,6 +1,6 @@
 __author__ = 'iway1'
 
-
+import numpy
 
 
 class AICA:
@@ -24,16 +24,29 @@ class AICA:
                     new_element = -1
                 new_row.append(new_element)
             self.cells.append(new_row)
+        self.cells = numpy.array(self.cells)
+
+    def distance(self, cell_1, cell_2):
+        dist_y = abs(cell_1[0] - cell_2[0])
+        dist_x = abs(cell_1[1] - cell_2[1])
+        if dist_y > self.N // 2:
+            dist_y = self.N - dist_y
+        if dist_x > self.N // 2:
+            dist_x = self.N - dist_x
+        return dist_y + dist_x
+
     def __getitem__(self, item):
         if type(item) != tuple or len(item) != 2:
             raise ValueError("Pass a tuple of length two to get item!")
-        return self.cells[item[0]][item[1]]
+        return self.cells[item]
     def __setitem__(self, key, value):
         if type(value) != int:
             raise ValueError("Only int values may be assigned to a cell.")
         if value != -1 and value != 1:
             raise ValueError("Cells may only take a value on of +/- 1.")
-        self.cells[key[0]][key[1]] = value
+        self.cells[key] = value
+    def __str__(self):
+        return str(self.cells)
 
 if __name__ == '__main__':
-    
+    ca = AICA("none", 30, None, None, None, None, None)
