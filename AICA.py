999e0aef1ce4aba3ad93427b4fddd83dfdf1f74a
iway1 <iway1@vols.utk.edu>
2018-02-25 16:23:47 -0500

    ...

--- AICA.py
+++ AICA.py
@@ -4,12 +4,36 @@ __author__ = 'iway1'
 
 
 class AICA:
-    def __init__(self, experiment_id, J1, J2, h, R1, R2):
+    def __init__(self, experiment_id, N, J1, J2, h, R1, R2):
         self.id = experiment_id
         self.J1 = J1
         self.J2 = J2
         self.h = h
         self.R1 = R1
         self.R2 = R2
+        self.N = N
+        self.init_cells()
     def init_cells(self):
-        
+        from random import randint
+        self.cells = []
+        for i in range(self.N):
+            new_row = []
+            for _ in range(self.N):
+                new_element = randint(0, 1)
+                if new_element == 0:
+                    new_element = -1
+                new_row.append(new_element)
+            self.cells.append(new_row)
+    def __getitem__(self, item):
+        if type(item) != tuple or len(item) != 2:
+            raise ValueError("Pass a tuple of length two to get item!")
+        return self.cells[item[0]][item[1]]
+    def __setitem__(self, key, value):
+        if type(value) != int:
+            raise ValueError("Only int values may be assigned to a cell.")
+        if value != -1 and value != 1:
+            raise ValueError("Cells may only take a value on of +/- 1.")
+        self.cells[key[0]][key[1]] = value
+
+if __name__ == '__main__':
+    
